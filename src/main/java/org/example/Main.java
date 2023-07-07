c016377307e550a3a8c929ced2c8364a0395520d
oliveiratamiris <tamirisocoelho@gmail.com>
2023-07-06 21:56:53 -0300

    Finalizado o exercício O maior do uri

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,32 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Locale.setDefault(Locale.US);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner sc =  new Scanner(System.in);
+
+        int  a, b, c, maiorParcial, maiorFinal;
+
+        a = sc.nextInt();
+
+        b =  sc.nextInt();
+
+        c = sc.nextInt();
+
+         maiorParcial = (a + b + Math.abs(a - b))/2;
+
+         maiorFinal =  (maiorParcial +  c + Math.abs(maiorParcial - c))/2;
+
+        System.out.println(maiorFinal + " eh o maior");
+
+        sc.close();
+
     }
 }
